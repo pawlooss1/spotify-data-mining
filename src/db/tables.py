f3a85969bd09afa3bb0a6ce24aaedc7bf5566f8c
Adam Księżyk <adam.ksiezyk@gmial.com>
2022-04-19 00:13:53 +0200

    Refactor gateway into pandas gateway
    Refactor tables
    
    Add countries table
    
    Refactor gateways to Pandas
    
    Add countries table
    
    Add countries gateway
    
    Remove country relation

--- src/db/tables.py
+++ src/db/tables.py
@@ -7,45 +7,56 @@ class WeeklyChart(Base):
     __tablename__ = "weekly_charts"
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    country = sqlalchemy.Column(sqlalchemy.String)
+    country_code = sqlalchemy.Column(sqlalchemy.VARCHAR(2),
+                                     sqlalchemy.ForeignKey("countries.id"))
+    # country = sqlalchemy.orm.relationship("Country", lazy="joined")
     date = sqlalchemy.Column(sqlalchemy.Date)
-    tracks = sqlalchemy.orm.relationship("ChartTrack", lazy="joined")
+    # tracks = sqlalchemy.orm.relationship("ChartTrack", lazy="joined")
+
+
+class Country(Base):
+    __tablename__ = "countries"
+
+    id = sqlalchemy.Column(sqlalchemy.VARCHAR(2), primary_key=True)
+    name = sqlalchemy.Column(sqlalchemy.String)
 
 
 class ChartTrack(Base):
     __tablename__ = "chart_tracks"
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    chart_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("weekly_charts.id"))
+    chart_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                 sqlalchemy.ForeignKey("weekly_charts.id"))
     position = sqlalchemy.Column(sqlalchemy.Integer)
-    track_id = sqlalchemy.Column(sqlalchemy.VARCHAR(22), sqlalchemy.ForeignKey("tracks.track_id"))
-    track = sqlalchemy.orm.relationship("Track", lazy="joined")
+    track_id = sqlalchemy.Column(sqlalchemy.VARCHAR(22),
+                                 sqlalchemy.ForeignKey("tracks.id"))
+    # track = sqlalchemy.orm.relationship("Track", lazy="joined")
     n_streams = sqlalchemy.Column(sqlalchemy.Integer)
 
 
 class Artist(Base):
     __tablename__ = "artists"
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    artist_id = sqlalchemy.Column(sqlalchemy.VARCHAR(length=22), unique=True)
+    id = sqlalchemy.Column(sqlalchemy.VARCHAR(length=22), primary_key=True)
     name = sqlalchemy.Column(sqlalchemy.String)
     followers = sqlalchemy.Column(sqlalchemy.Integer)
     genres = sqlalchemy.Column(sqlalchemy.JSON)
     popularity = sqlalchemy.Column(sqlalchemy.Integer)
 
 
-track_artists = sqlalchemy.Table("track_artists", Base.metadata,
-                                 sqlalchemy.Column("track_id", sqlalchemy.ForeignKey("tracks.track_id")),
-                                 sqlalchemy.Column("artist_id", sqlalchemy.ForeignKey("artists.artist_id")))
+track_artists = sqlalchemy.Table("track_artists",
+                                 Base.metadata,
+                                 sqlalchemy.Column(
+                                     "track_id", sqlalchemy.ForeignKey("tracks.id")),
+                                 sqlalchemy.Column("artist_id", sqlalchemy.ForeignKey("artists.id")))
 
 
 class Track(Base):
     __tablename__ = "tracks"
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    track_id = sqlalchemy.Column(sqlalchemy.VARCHAR(length=22), unique=True)
+    id = sqlalchemy.Column(sqlalchemy.VARCHAR(length=22), primary_key=True)
     title = sqlalchemy.Column(sqlalchemy.String)
-    artists = sqlalchemy.orm.relationship("Artist", secondary=track_artists)
+    # artists = sqlalchemy.orm.relationship("Artist", secondary=track_artists)
     genres = sqlalchemy.Column(sqlalchemy.JSON)
     danceability = sqlalchemy.Column(sqlalchemy.Float)
     energy = sqlalchemy.Column(sqlalchemy.Float)
