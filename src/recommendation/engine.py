01312a6f8bfa3591a37b88870438d8461addb257
dittohed <wojtek.konieczkowicz@gmail.com>
2022-06-05 00:43:08 +0200

    Updated code basing on the demo notebook

--- src/recommendation/engine.py
+++ src/recommendation/engine.py
@@ -7,9 +7,10 @@ from sklearn.preprocessing import MinMaxScaler
 
 from db.gateway import TracksGateway
 from domain.track import AudioFeatures
+from recommendation import UserVector
 
 FEATURE_NAMES = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness',
-                 'liveness', 'valence', 'tempo', 'duration_ms', 'time_signature']
+                 'liveness', 'valence', 'tempo', 'duration_ms', 'time_signature', 'n_streams']
 CAT_FEATURES = ['key', 'mode', 'time_signature']
 CAT_COLUMN_NAMES = ['key_0', 'key_1', 'key_2', 'key_3', 'key_4', 'key_5', 'key_6', 'key_7', 'key_8', 'key_9', 'key_10',
                     'key_11', 'mode_0', 'mode_1', 'time_signature_0', 'time_signature_1', 'time_signature_2',
@@ -39,29 +40,27 @@ def preprocess_features(features_df: pd.DataFrame) -> pd.DataFrame:
 features_scaled = preprocess_features(features)
 
 
-def get_similarities(tracks_user: pd.DataFrame, beta: float) -> np.ndarray:
-    tracks_user = preprocess_features(tracks_user).to_numpy()
-    tracks_user = scaler.transform(tracks_user)
+def get_similarities(user_vector: UserVector, popularity_rate: float = 0):
+    n_streams = features_scaled.n_streams.to_numpy().reshape(-1, 1)
+    features_scaled = features_scaled.drop(['n_streams'], axis=1)
 
-    # Exponential Moving Average with bias correction
-    user_vector = 0
-    for t in range(1, tracks_user.shape[0]+1):
-        user_vector = (
-            (beta*user_vector + (1-beta)*tracks_user[t-1]) / (1-beta**t))
+    audio_similarities = cosine_similarity(
+                            features_scaled, user_vector.vec)
+    similarities = audio_similarities + popularity_rate*n_streams
 
-    return cosine_similarity(features_scaled, user_vector.reshape(1, -1))
+    return similarities
 
 
 def create_recommendations(tracks_user: List[AudioFeatures]) -> List[str]:
     tracks_user_df = pd.DataFrame(tracks_user)
-    similarities = get_similarities(tracks_user_df, 0.9)
+    user_vector = UserVector(tracks_user, scaler)
+
+    similarities = get_similarities(user_vector)
     tracks_with_similarities = db_mock.assign(similarity=similarities)
 
     # Drop tracks that are already there in user history
     # to not recommend what user listened to already
     tracks_with_similarities.drop(tracks_user_df.index, inplace=True)
 
-    similarities = get_similarities(tracks_user_df)
-    tracks_with_similarities = features.assign(similarity=similarities)
     top_recommendations = tracks_with_similarities.sort_values('similarity', ascending=False).iloc[:10]
     return [track_id for track_id in top_recommendations.index]
